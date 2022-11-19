c1ad66aa5a8d5d80d1f219b040a40f68b6d20022
neslihan <20060376@stu.omu.edu.tr>
2022-11-19 16:56:00 +0300

    first day

---
+++ ProductApp/ProductApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        string message = "Hello MVC";
+
+        return View("Index", message);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult GetProducts()
+    {
+        Product product = new Product(1, "Computer", 1000);
+        Product product1 = new Product(2, "Phone", 2000);
+
+        var products = new List<Product>();
+        products.Add(product);
+        products.Add(product1);
+
+        return View("GetProduct", products);
+    }
+
+}
---
+++ ProductApp/ProductApp/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(int ıd, string name, decimal price)
+        {
+            Id = ıd;
+            Name = name;
+            Price = price;
+        }
+    }
+
+}
